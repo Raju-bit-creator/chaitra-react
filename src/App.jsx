e6f865837b4a6706d68c614d92e3655b803828c5
Raju <rajutamang3456@gmail.com>
2025-04-28 16:53:22 +0545

    react components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
 
 function App() {
   const [count, setCount] = useState(10);
@@ -9,14 +11,26 @@ function App() {
     setCount(count + 5);
   };
 
+  const handleDecrement = () => {
+    setCount(count - 5);
+  };
+  const handleMultiplication = () => {
+    setCount(count * 5);
+  };
+
   return (
     <>
-      <div className="card">
+      <Navbar />
+      <Banner />
+      <div className="container mt-5">
         <button onClick={handleIncrement}>click me to increase</button>
-        <button onClick={() => setCount((count) => count - 5)}>
-          click me to decrease
-        </button>
+        <button onClick={handleDecrement}>click me to decrease</button>
+        <button onClick={handleMultiplication}>click me to multiply</button>
+
         <p>you click {count} time in click me button</p>
+      </div>
+      <div className="container">
+        <h4>Welcome back to login page</h4>
       </div>
     </>
   );
