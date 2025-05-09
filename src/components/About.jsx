364cfe4dc3894f8bd3c996cb58cd12b6d0bc8961
Raju <rajutamang3456@gmail.com>
2025-05-09 16:43:24 +0545

    context2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,17 +1,34 @@
 import React, { useContext } from "react";
 import Card from "./card";
 import productContext from "../context/ProductContext";
+import img from "../assets/img.jpg";
 
 const About = () => {
   const context = useContext(productContext);
-  const { product } = context;
-  console.log("fruit name is ", product);
+  const { title, price, description } = context; //destructuring
+  console.log("fruit name is ", description);
 
   return (
-    <div>
-      this is about page
-      <h4>this not phone this fruit and name is {product}</h4>
-      <Card />
+    <div className="container">
+      <div className="row">
+        this is about page
+        <h4>this is product for contexts {title}</h4>
+        <h2>Our product</h2>
+        <div className="col-md-4">
+          <div className="card">
+            <img src={img} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">{description}</p>
+              <p>Price: Rs{price}</p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+        <Card />
+      </div>
     </div>
   );
 };
--- src/context/ProductState.jsx
+++ src/context/ProductState.jsx
@@ -2,7 +2,42 @@ import React from "react";
 import ProductContext from "./ProductContext";
 
 const ProductState = (props) => {
-  const product = "apple";
+  const product = {
+    title: "apple",
+    price: 100,
+    description: "This is an apple from mustang",
+  };
+
+  const products = [
+    {
+      _id: 1,
+      title: "apple",
+      description: "this is good product",
+      price: 100,
+      instock: 5,
+    },
+    {
+      _id: 2,
+      title: "mango",
+      description: "this is good product from terai",
+      price: 300,
+      instock: 5,
+    },
+    {
+      _id: 3,
+      title: "Grapes",
+      description: "this is good product local farm",
+      price: 500,
+      instock: 3,
+    },
+    {
+      _id: 4,
+      title: "Banana",
+      description: "this is good for health",
+      price: 50,
+      instock: 10,
+    },
+  ];
   return (
     <ProductContext.Provider value={product}>
       {props.children}
