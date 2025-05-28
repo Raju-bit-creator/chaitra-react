103c896a49d16072d80a6f33cd19c553e64ccd53
Raju <rajutamang3456@gmail.com>
2025-05-28 16:54:41 +0545

    schema

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./db");
+const { chats } = require("./data/data");
 const app = express();
 
 dotenv.config();
@@ -16,6 +17,16 @@ app.get("/about", (req, res) => {
 app.get("/contact/address", (req, res) => {
   res.send("i am from kathmandu");
 });
+app.get("/chats", (req, res) => {
+  res.send({ chats });
+});
+app.get("/chats/:id", (req, res) => {
+  // console.log(req.params.id);
+  const singleChat = chats.find((chat) => chat._id === req.params.id);
+  res.send({ singleChat });
+});
+
+app.use("/api/auth", require("./routes/auth"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
