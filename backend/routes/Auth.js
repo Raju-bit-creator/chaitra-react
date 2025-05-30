3be230c6f57260c2418b7f67c587d0a207150a86
Raju <rajutamang3456@gmail.com>
2025-05-30 16:45:28 +0545

    hash user

--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -1,28 +1,57 @@
 const express = require("express");
 const User = require("../model/User");
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { body, validationResult } = require("express-validator");
+
+const secret = process.env.SECRET;
 
 router.get("/users", (req, res) => {
   res.send("all user from database");
 });
-router.post("/createuser", async (req, res) => {
-  try {
-    let user = await User.findOne({ email: req.body.email });
-    if (user) {
-      return res.status(400).send({ message: "user already exist" });
+router.post(
+  "/createuser",
+  [
+    body("name")
+      .isLength({ min: 3 })
+      .withMessage("name must be atleast 3 character"),
+    body("email").isEmail().withMessage("invalid email"),
+    body("password")
+      .isLength({ min: 5 })
+      .withMessage("passsword must be atleast 5 character"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    user = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
-    res.json({ user });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send("internal server error");
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).send({ message: "user already exist" });
+      }
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
+      });
+      const data = {
+        user: {
+          id: user._id,
+        },
+      };
+      const authToken = jwt.sign(data, secret);
+      // console.log(authToken);
+
+      res.json({ user, authToken });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("internal server error");
+    }
   }
-});
+);
 
 module.exports = router;
